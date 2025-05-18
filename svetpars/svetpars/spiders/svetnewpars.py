9e96b16169650125c4b7bdb478282092a7864059
Lanuta <a_aleksandrova@bk.ru>
2025-05-18 15:41:13 +0300

    Spider
    1. установлена библиотека
    2. создан шаблон (scrapy startproject svetpars)
    3.файл внутри пакета spiders
    4.Разработка spider

---
+++ svetpars/svetpars/spiders/svetnewpars.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class SvetnewparsSpider(scrapy.Spider):
+    name = "svetnewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        svets = response.css('div.WdR1o')
+        for svet in svets:
+            yield {
+                'name': svet.css('div.lsooF span::text').get(),
+                'price': svet.css('div.pY3d2 span::text').get(),
+                'url': svet.css('a').attrib['href']
+            }
