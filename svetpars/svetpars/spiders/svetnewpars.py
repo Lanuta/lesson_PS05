76e70f37400d31e82de9481cfbd6b254efcd0b6e
Lanuta <a_aleksandrova@bk.ru>
2025-05-22 22:34:23 +0300

    lesson_PS06
    сохранение данных в файл csv

--- svetpars/svetpars/spiders/svetnewpars.py
+++ svetpars/svetpars/spiders/svetnewpars.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import csv
 
 class SvetnewparsSpider(scrapy.Spider):
     name = "svetnewpars"
@@ -7,10 +7,18 @@ class SvetnewparsSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
+        parsed_data = []
         svets = response.css('div.WdR1o')
         for svet in svets:
-            yield {
-                'name': svet.css('div.lsooF span::text').get(),
-                'price': svet.css('div.pY3d2 span::text').get(),
-                'url': svet.css('a').attrib['href']
-            }
+
+                name = svet.css('div.lsooF span::text').get(),
+                price = svet.css('div.pY3d2 span::text').get(),
+                url = svet.css('a::attr(href)').get()
+                url = response.urljoin(url)
+
+                parsed_data.append([name, price, url])
+
+        with open("svet.csv", 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Название источника света', 'цена', 'Ссылка'])
+            writer.writerows(parsed_data)
